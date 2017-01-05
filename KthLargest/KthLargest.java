f99279831c2330de588106813bab4477d7f9cc43
CasSinders <Gloomgamer@gmail.com>
2017-01-05 00:56:31 -0500

    Added kthLargest2
    Added a second version of finding out what the kth largest element one.

--- KthLargest/KthLargest.java
+++ KthLargest/KthLargest.java
@@ -4,11 +4,11 @@ import java.util.Comparator;
 /**
  * TODO: Report the results of your time trials here.
  *  
- * @author <insert your name here>
+ * @author <Christopher Sinders>
  */
 
 public class KthLargest {
-
+  
   /**
    * Weiss (pg 1) Suppose you have a group of N numbers and would like to
    * determine the kth largest. This is known as the selection problem.
@@ -17,7 +17,7 @@ public class KthLargest {
    * sort the array in decreasing order by some simple algorithm such as
    * bubblesort, and then return the element in position k.
    */
-
+  
   public static int kthLargest1(int[] a, int k) {
     assert a != null;
     int n = a.length;
@@ -32,7 +32,7 @@ public class KthLargest {
     });
     return b[k - 1];
   }
-
+  
   /**
    * Weiss (pg 1) A somewhat better algorithm might be to read the first k
    * elements into an array and sort them (in decreasing order). Next, each
@@ -42,10 +42,71 @@ public class KthLargest {
    * array. When the algorithm ends, the element in the kth position is returned
    * as the answer.
    */
-
+  
   public static int kthLargest2(int[] a, int k) {
-    // TODO
-    return 0;
+    assert a != null;
+    System.out.println("a is not null");
+    int n = a.length;
+    assert n > 0 && k <= n;
+    System.out.println("n is > 0 and k <= n");
+    Integer[] b = new Integer[k];
+    System.out.println("The size of b is " + b.length);
+    
+    // Place the first k number elements into the array
+    for (int i = 0; i < k; i++){
+      b[i] = a[i];
+    }
+    System.out.println("sucessfully placed the first " + k + " elements into the array");
+    for (int i = 0; i < k; i++) {
+      System.out.print(b[i] + " ");
+    }
+    System.out.println();
+    
+    // Sort the elements in descending order using selection sort
+    for (int i = 0; i < k; i++){
+      int indexMax = i;
+      for (int j = i+1; j < k; j++) {
+        if (b[indexMax] < b[j]) {
+          indexMax = j;
+        }
+      }
+      int temp  = b[indexMax];
+      b[indexMax] = b[i];
+      b[i] = temp;
+    }
+    System.out.println("b was sucessfully sorted"); 
+    for (int i = 0; i < k; i++) {
+      System.out.print(b[i] + " ");
+    }
+    System.out.println();
+    
+    
+    // Read through the remaining elements
+    for (int nextElement = k; nextElement < n; nextElement++) {
+      System.out.println("checking the next element " + a[nextElement] + " > " + b[k-1] + ",  my smallest element");
+      if (a[nextElement] > b[k-1]) {
+        // the next element is bigger than my smallest element, so insert it into the array
+        int insertAt = 0;
+        // Find out where to insert the new element
+        while (b[insertAt] > a[nextElement]) {
+          insertAt++;
+        }
+        // Shift everything from where we are inserting, up the array
+        for (int i = k-1; i > insertAt; i--) {
+          b[i] = b[i-1];
+        }
+        // insert the new element into the array
+        b[insertAt] = a[nextElement];
+      }
+      System.out.println("the Array is now ");
+      for (int i = 0; i < b.length; i++) {
+        System.out.print(b[i] + " ");
+      }
+      System.out.println();
+      
+    }
+    System.out.println("Finish finding the " + k + "the biggest element, " + b[k-1]);
+    return b[k-1];
   }
   
   /**
@@ -60,9 +121,27 @@ public class KthLargest {
   public static void main(String[] args) {
     int[] a;
     a = new int[] { 8, 7, -3, 9, 2, 1, -5, 4, 12, -2 };
+    // intital asserts
     assert kthLargest1(a, 1) == 12;
     assert kthLargest1(a, 2) == 9;
     assert kthLargest1(a, 3) == 8;
+    // Added Additional asserts
     assert kthLargest1(a, 4) == 7;
+    assert kthLargest1(a, 5) == 4;
+    assert kthLargest1(a, 6) == 2;
+    assert kthLargest1(a, 7) == 1;
+    assert kthLargest1(a, 8) == -2;
+    assert kthLargest1(a, 9) == -3;
+    
+    
+    
+        assert kthLargest2(a, 1) == 12;
+    assert kthLargest2(a, 2) == 9;
+    assert kthLargest2(a, 3) == 8;
+    assert kthLargest2(a, 4) == 7;
+    assert kthLargest2(a, 5) == 4;
+    assert kthLargest2(a, 7) == 1;
+    assert kthLargest2(a, 8) == -2;
+    assert kthLargest2(a, 9) == -3;
   }
 }
